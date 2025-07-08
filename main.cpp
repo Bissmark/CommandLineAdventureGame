328e821cd4bd58a26e1ea0fd1b80634e2d8e53fe
Christopher Holt <holt.christopher1@gmail.com>
2025-07-08 13:43:12 +1000

    first commit, command line c++ adventure game

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string charName;
+
+    cout << "-----------------------------------------------------------------------------" << endl;
+    cout << "- Welcome to Adventure Game, First please create a name for your character: -" << endl;
+    cin >> charName;
+
+    cout << "Welcome to ____ Land " << charName << endl;
+
+    cout << "\n " << endl;
+    return 0;
+}
